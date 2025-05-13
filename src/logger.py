0fb78194fd0aa4343599fb64223c02952bf2fb52
alon <alonsuissa12@gmail.com>
2025-05-13 22:29:07 +0300

    modular logger

--- src/logger.py
+++ src/logger.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 
 # Create logs directory if it doesn't exist
-def setup_logger():
+def setup_logger(name):
     # Create logs directory if it doesn't exist
     if not os.path.exists('logs'):
         os.makedirs('logs')
 
     # Create a unique log file name with timestamp
-    log_filename = f"logs/clalit_run_{datetime.now().strftime('%Y-%m-%d__time_%H-%M')}.log"
+    log_filename = f"logs/{name}__{datetime.now().strftime('%Y-%m-%d___time__%H-%M')}.log"
 
     # Configure the logger
     logging.basicConfig(
